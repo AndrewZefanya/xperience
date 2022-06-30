fcfa0a517469a17a701dab1c32ac2fac099a78ed
Andrew Zefanya <andrewz2002kapuas26@gmail.com>
2022-07-01 03:13:19 +0700

    install and integrate socialite

--- xperience/routes/web.php
+++ xperience/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,6 +30,8 @@ Route::get('/success_checkout', function () {
     return view('success_checkout');
 })->middleware(['auth'])->name('success-checkout');
 
-
+// socialite routes
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
 require __DIR__.'/auth.php';
